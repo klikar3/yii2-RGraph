84b3b63700e30552d4e16e134f8fa7e27b6701eb
klikar3 <klikar3@users.noreply.github.com>
2016-02-11 17:51:03 +0100

    Update RGraphLine.php

--- RGraphLine.php
+++ RGraphLine.php
@@ -18,15 +18,15 @@ class RGraphLine extends RGraphWidget
 {
 	public function init()
 	{
-	$id = $this->getId();
-	if (isset($this->htmlOptions['id']))
-		$id = $this->htmlOptions['id'];
-	else
-		$this->htmlOptions['id'] = $id;
-		
-	if (!isset($this->options['id'])) {
-		$this->options['id'] = $id;
-	}
+		$id = $this->getId();
+		if (isset($this->htmlOptions['id']))
+			$id = $this->htmlOptions['id'];
+		else
+			$this->htmlOptions['id'] = $id;
+			
+		if (!isset($this->options['id'])) {
+			$this->options['id'] = $id;
+		}
     
 		parent::init();
 		$this->registerScriptFile('RGraph.line.js');
