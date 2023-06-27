753c070a71a6701fa8e55039ac8ecea98856a4ce
Klingler-Admin <KlinglKa-Adm@intra.ads-root.de>
2023-06-27 16:47:44 +0200

    -- yidas/yii2-composer-bower-skip ersetzt durch AssesPackagist
    Signed-off-by: Klingler-Admin <KlinglKa-Adm@intra.ads-root.de>

--- vendor/yiisoft/extensions.php
+++ vendor/yiisoft/extensions.php
@@ -3,9 +3,4 @@
 $vendorDir = dirname(__DIR__);
 
 return array (
-  'yidas/yii2-composer-bower-skip' => 
-  array (
-    'name' => 'yidas/yii2-composer-bower-skip',
-    'version' => '2.0.13.0',
-  ),
 );
